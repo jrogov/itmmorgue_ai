7276b6ec9ae465a3d99260268b9b1c11729c0ca9
Yaroslav Rogov <rogovyaroslav@gmail.com>
2018-03-06 20:35:32 +0700

    Change DEBUG_* stubs

--- common.h
+++ common.h
@@ -86,24 +86,38 @@ extern int DEBUG_STARTPOS_X;
                                         } while(0)
 
 
+#define DEBUG_WAIT_PRESS()               wgetch(DEBUG_WINDOW)
+
 #else
 
 // I will be beaten up for this
 void null_varargs(int i, ...);
-#define void_varargs(a, ...) if(0){ null_varargs(0, __VA_ARGS__);};
+#define void_varargs(...) if(0){ null_varargs(0, __VA_ARGS__);};
 
 // A pack of stubs that does nothing
-#define DEBUG_INIT(height, width, y, x) do { (void) (height); (void) (width); (void) (y); (void) (x); } while(0)
-#define DEBUG_FIXPOS()                  do {} while(0)
-#define DEBUG_STARTPOS(y, x)            do { (void) (y); (void) (x); } while(0)
-#define DEBUG_RESET()                   do {} while(0)
-#define DEBUG_IMPRINTF(y, x, s, ...)    do { (void) (y); (void) (x); (void) (s); void_varargs(0, __VA_ARGS__); } while(0)
-#define DEBUG_IMPRINT(y, x, s)          do { (void) (y); (void) (x); (void) (s); } while(0)
-#define DEBUG_MPRINTF(y, x, s, ...)     do { (void) (y); (void) (x); (void) (s); void_varargs(0, __VA_ARGS__); } while(0)
-#define DEBUG_MPRINT(y, x, s)           do { (void) (y); (void) (x); (void) (s); } while(0)
-#define DEBUG_PRINTF(s, ...)            do { (void) (s); void_varargs(0, __VA_ARGS__); } while(0)
-#define DEBUG_PRINT(s)                  do { (void) (s); } while(0)
-#define DEBUG_REFRESH()                 do {} while(0)
+#define DEBUG_DO_NOTHING()                      do{} while(0)                                                        
+#define DEBUG_DO_NOTHING_UNUSED_1(a)                  do{ (void)(a); } while(0)                                            
+#define DEBUG_DO_NOTHING_UNUSED_2(a,b)                do{ (void)(a); (void)(b); } while(0)                                 
+#define DEBUG_DO_NOTHING_UNUSED_3(a,b,c)              do{ (void)(a); (void)(b); (void)(c); } while(0)                      
+#define DEBUG_DO_NOTHING_UNUSED_4(a,b,c,d)            do{ (void)(a); (void)(b); (void)(c); (void)(d); } while(0)                      
+#define DEBUG_DO_NOTHING_UNUSED_1_VA(a, ...)          do{ DEBUG_DO_NOTHING_UNUSED_1(a); void_varargs(__VA_ARGS__); } while(0)    
+#define DEBUG_DO_NOTHING_UNUSED_2_VA(a, b, ...)       do{ DEBUG_DO_NOTHING_UNUSED_2(a,b); void_varargs(__VA_ARGS__); } while(0)  
+#define DEBUG_DO_NOTHING_UNUSED_3_VA(a, b, c, ...)    do{ DEBUG_DO_NOTHING_UNUSED_3(a,b,c); void_varargs(__VA_ARGS__); } while(0)
+#define DEBUG_DO_NOTHING_UNUSED_4_VA(a, b, c, d, ...) do{ DEBUG_DO_NOTHING_UNUSED_3(a,b,c,d); void_varargs(__VA_ARGS__); } while(0)
+
+
+#define DEBUG_INIT(height, width, y, x) DEBUG_DO_NOTHING_UNUSED_4((height),(width),(y),(x))
+#define DEBUG_FIXPOS()                  DEBUG_DO_NOTHING()
+#define DEBUG_STARTPOS(y, x)            DEBUG_DO_NOTHING_UNUSED_2((y),(x))
+#define DEBUG_RESET()                   DEBUG_DO_NOTHING()
+#define DEBUG_IMPRINTF(y, x, s, ...)    DEBUG_DO_NOTHING_UNUSED_3_VA((y),(x),(s), __VA_ARGS__)
+#define DEBUG_IMPRINT(y, x, s)          DEBUG_DO_NOTHING_UNUSED_3_VA((y),(x),(s))
+#define DEBUG_MPRINTF(y, x, s, ...)     DEBUG_DO_NOTHING_UNUSED_3_VA((y),(x),(s), __VA_ARGS__)
+#define DEBUG_MPRINT(y, x, s)           DEBUG_DO_NOTHING_UNUSED_3_VA((y),(x),(s))
+#define DEBUG_PRINTF(s, ...)            DEBUG_DO_NOTHING_UNUSED_1_VA((s), __VA_ARGS__)
+#define DEBUG_PRINT(s)                  DEBUG_DO_NOTHING_UNUSED_1(s)
+#define DEBUG_REFRESH()                 DEBUG_DO_NOTHING()
+#define DEBUG_WAIT_PRESS()              DEBUG_DO_NOTHING()
 
 #endif /* DEBUG */
 
